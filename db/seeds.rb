2dd93ded965fce4cad21c36120885ce5f92d5ce1
Leonard Siebold <leo.siebold@hotmail.de>
2019-05-15 17:17:29 +0200

    started but not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Amazing Frankfurt',
+  address: 'Sachsenhausen',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Garden Flat New York',
+  address: 'Broadway',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 1000,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Barcelona',
+  address: 'Gracia',
+  description: 'Large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 3
+)
